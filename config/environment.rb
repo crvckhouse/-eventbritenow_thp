02c2a2875feaacb145a63cc6f09f0f04ebfa18bb
ilyes sedjal <ilyes.sedjal@gmail.com>
2022-02-21 15:48:49 +0100

    testeur run

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'form4s.herokuapp.com',
+  :domain => 'gmail.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
